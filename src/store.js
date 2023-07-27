dcac67610b19a77a774cb8b6c2f30ec9ae4120f1
Grégory Marini <greg.marini@hotmail.fr>
2023-07-27 17:13:54 +0200

    finish using redux, start to add redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,13 +1,10 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux';
-import thunk from 'redux-thunk';
 import customerReducer from './features/customers/customerSlice';
 import accountReducer from './features/accounts/accountSlice';
 
-const rootReducer = combineReducers({
-  customer: customerReducer,
-  account: accountReducer,
-});
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
+});
 
 export default store;
